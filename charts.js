8875e751fe508f33859ad8e2c31b5d2a04d433f9
adamad44 <adam.python3@gmail.com>
2025-08-01 10:13:42 +0200

    Fix: Reverse muscle group over time data sorting for correct chronological display

--- charts.js
+++ charts.js
@@ -1109,6 +1109,7 @@ export async function renderMuscleGroupOverTimeChart() {
   const muscleGroupOverTimeData = getSetCountsByMuscleGroupOverTime();
 
   muscleGroupOverTimeData.sort((a, b) => new Date(a.month) - new Date(b.month));
+  muscleGroupOverTimeData.reverse();
   const months = muscleGroupOverTimeData.map((item) => item.month);
 
   const allMuscleGroups = [];
